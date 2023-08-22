55cb537e8b0cddfb717422fc3f2023d83a205574
xmdhs <xmdhss@gmail.com>
2023-08-22 23:51:30 +0800

    fix

--- convert/hysteria.go
+++ convert/hysteria.go
@@ -21,7 +21,7 @@ func hysteria(p *clash.Proxies, s *singbox.SingBoxOut) error {
 	}
 	s.Obfs = p.Obfs
 	s.TLS.Alpn = p.Alpn
-	if p.Protocol != "udp" || p.Protocol == "" {
+	if p.Protocol != "udp" && p.Protocol != "" {
 		return fmt.Errorf("hysteria: %w", ErrNotSupportType)
 	}
 	if up, err := strconv.Atoi(p.Up); err == nil {
