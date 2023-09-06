5eb3ac39d5727ae321a89deb0813650fcbe7577a
xmdhs <xmdhss@gmail.com>
2023-09-06 22:17:25 +0800

    fix tuic

--- convert/tuic.go
+++ convert/tuic.go
@@ -10,9 +10,13 @@ import (
 func tuic(p *clash.Proxies, s *singbox.SingBoxOut) ([]singbox.SingBoxOut, error) {
 	p.Tls = true
 	tls(p, s)
+	s.UUID = p.Uuid
 	s.CongestionController = p.CongestionController
 	s.UdpRelayMode = p.UdpRelayMode
 	s.ZeroRttHandshake = p.ReduceRtt
 	s.Heartbeat = fmt.Sprintf("%vms", p.HeartbeatInterval)
+	if p.IP != "" {
+		s.Server = p.IP
+	}
 	return []singbox.SingBoxOut{*s}, nil
 }
