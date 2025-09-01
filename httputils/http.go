7b237eef77867e2c5e23d904d80a8a88b5808436
xmdhs <xmdhss@gmail.com>
2025-09-02 01:58:30 +0800

    Update http.go

--- httputils/http.go
+++ httputils/http.go
@@ -13,7 +13,7 @@ func HttpGet(cxt context.Context, c *http.Client, url string, maxByte int64) ([]
 		return nil, fmt.Errorf("HttpGet: %w", err)
 	}
 	reqs.Header.Set("Accept", "*/*")
-	reqs.Header.Set("User-Agent", "clash2singbox (sing-box 1.12.0 SFA Clash ClashMeta clash.meta)")
+	reqs.Header.Set("User-Agent", "sing-box 1.12.0 (ClashMetaForAndroid) clash2singbox")
 	rep, err := c.Do(reqs)
 	if rep != nil {
 		defer rep.Body.Close()
