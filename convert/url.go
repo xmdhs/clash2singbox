124491ee15457626813124a53a0f9c8fe91cbe4a
xmdhs <xmdhss@gmail.com>
2025-11-24 17:30:28 +0800

    原始格式解析

---
+++ convert/url.go
@@ -0,0 +1,499 @@
+package convert
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/xmdhs/clash2singbox/model/clash"
+)
+
+func ParseURL(s string) (clash.Proxies, error) {
+	s = strings.TrimSpace(s)
+	u, err := url.Parse(s)
+	if err != nil {
+		return clash.Proxies{}, err
+	}
+	var p clash.Proxies
+	switch u.Scheme {
+	case "ss":
+		p, err = parseSs(u)
+	case "vmess":
+		p, err = parseVmess(s)
+	case "vless":
+		p, err = parseVless(u)
+	case "trojan":
+		p, err = parseTrojan(u)
+	case "hysteria":
+		p, err = parseHysteria(u)
+	case "hy2", "hysteria2":
+		p, err = parseHysteria2(u)
+	case "tuic":
+		p, err = parseTuic(u)
+	case "socks5":
+		p, err = parseSocks5(u)
+	case "http", "https":
+		p, err = parseHttp(u)
+	default:
+		return clash.Proxies{}, fmt.Errorf("unsupported protocol: %s", u.Scheme)
+	}
+	if err != nil {
+		return clash.Proxies{}, fmt.Errorf("%s: %w", s, err)
+	}
+	return p, nil
+}
+
+func parseHttp(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Name:   u.Fragment,
+		Type:   "http",
+		Server: u.Hostname(),
+		Port:   u.Port(),
+	}
+	if u.User != nil {
+		p.Username = u.User.Username()
+		pass, ok := u.User.Password()
+		if ok {
+			p.Password = pass
+		}
+	}
+	if u.Scheme == "https" {
+		p.Tls = clash.MyBool(true)
+	}
+	return p, nil
+}
+
+func parseSocks5(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Name:   u.Fragment,
+		Type:   "socks5",
+		Server: u.Hostname(),
+		Port:   u.Port(),
+	}
+	if u.User != nil {
+		p.Username = u.User.Username()
+		pass, ok := u.User.Password()
+		if ok {
+			p.Password = pass
+		}
+	}
+	return p, nil
+}
+
+func parseTuic(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Name:     u.Fragment,
+		Type:     "tuic",
+		Server:   u.Hostname(),
+		Port:     u.Port(),
+		Password: u.User.Username(),
+	}
+	q := u.Query()
+	if sni, ok := q["sni"]; ok {
+		p.Sni = sni[0]
+	}
+	if alpn, ok := q["alpn"]; ok {
+		p.Alpn = strings.Split(alpn[0], ",")
+	}
+	if scv, ok := q["skip-cert-verify"]; ok && len(scv) > 0 && (scv[0] == "true" || scv[0] == "1") {
+		p.SkipCertVerify = clash.MyBool(true)
+	}
+	if ds, ok := q["disable-sni"]; ok && len(ds) > 0 && (ds[0] == "true" || ds[0] == "1") {
+		p.DisableSni = clash.MyBool(true)
+	}
+	if cr, ok := q["congestion-controller"]; ok {
+		p.CongestionController = cr[0]
+	}
+	if um, ok := q["udp-relay-mode"]; ok {
+		p.UdpRelayMode = um[0]
+	}
+	if rr, ok := q["reduce-rtt"]; ok && len(rr) > 0 && (rr[0] == "true" || rr[0] == "1") {
+		p.ReduceRtt = clash.MyBool(true)
+	}
+	if hi, ok := q["heartbeat-interval"]; ok {
+		i, err := strconv.Atoi(hi[0])
+		if err == nil {
+			p.HeartbeatInterval = clash.MyInt(i)
+		}
+	}
+	return p, nil
+}
+
+func parseHysteria2(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Name:     u.Fragment,
+		Type:     "hysteria2",
+		Server:   u.Hostname(),
+		Port:     u.Port(),
+		Password: u.User.String(),
+	}
+	q := u.Query()
+	if scv, ok := q["insecure"]; ok && len(scv) > 0 && scv[0] == "1" {
+		p.SkipCertVerify = clash.MyBool(true)
+	}
+	if sni, ok := q["sni"]; ok {
+		p.Sni = sni[0]
+	}
+	if obfs, ok := q["obfs"]; ok {
+		p.Obfs = obfs[0]
+	}
+	if op, ok := q["obfs-password"]; ok {
+		p.ObfsPassword = op[0]
+	}
+	if mport, ok := q["mport"]; ok {
+		p.Ports = mport[0]
+	}
+	return p, nil
+}
+
+func parseHysteria(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Name:   u.Fragment,
+		Type:   "hysteria",
+		Server: u.Hostname(),
+		Port:   u.Port(),
+	}
+	q := u.Query()
+	if alpn, ok := q["alpn"]; ok {
+		p.Alpn = strings.Split(alpn[0], ",")
+	}
+	if scv, ok := q["insecure"]; ok && len(scv) > 0 && (scv[0] == "1" || scv[0] == "true") {
+		p.SkipCertVerify = clash.MyBool(true)
+	}
+	if auth, ok := q["auth"]; ok {
+		p.AuthStr = auth[0]
+	}
+	if ports, ok := q["mport"]; ok {
+		p.Ports = ports[0]
+	}
+	if op, ok := q["obfsParam"]; ok {
+		p.Obfs = op[0]
+	}
+	if up, ok := q["upmbps"]; ok {
+		p.Up = up[0]
+	}
+	if down, ok := q["downmbps"]; ok {
+		p.Down = down[0]
+	}
+	if obfs, ok := q["obfs"]; ok {
+		p.Obfs = obfs[0]
+	}
+	if fo, ok := q["fast-open"]; ok && len(fo) > 0 && (fo[0] == "1" || fo[0] == "true") {
+		p.FastOpen = clash.MyBool(true)
+	}
+	if rwc, ok := q["recv-window-conn"]; ok {
+		i, err := strconv.Atoi(rwc[0])
+		if err == nil {
+			p.RecvWindowConn = clash.MyInt(i)
+		}
+	}
+	if rw, ok := q["recv-window"]; ok {
+		i, err := strconv.Atoi(rw[0])
+		if err == nil {
+			p.RecvWindow = clash.MyInt(i)
+		}
+	}
+	if dmd, ok := q["disable-mtu-discovery"]; ok && len(dmd) > 0 && (dmd[0] == "1" || dmd[0] == "true") {
+		p.DisableMtuDiscovery = clash.MyBool(true)
+	}
+	if fp, ok := q["fingerprint"]; ok {
+		p.Fingerprint = fp[0]
+	}
+	if protocol, ok := q["protocol"]; ok {
+		p.Protocol = protocol[0]
+	}
+	if sni, ok := q["sni"]; ok {
+		p.Sni = sni[0]
+	}
+
+	return p, nil
+}
+
+func parseTrojan(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Type:   "trojan",
+		Name:   u.Fragment,
+		Server: u.Hostname(),
+		Port:   u.Port(),
+	}
+	if u.User != nil {
+		p.Username = u.User.Username()
+	}
+
+	q := u.Query()
+	if t, ok := q["type"]; ok {
+		p.Network = t[0]
+	}
+	if host, ok := q["host"]; ok {
+		p.WsOpts.Headers = map[string]string{
+			"Host": host[0],
+		}
+	}
+	if path, ok := q["path"]; ok {
+		p.WsOpts.Path = path[0]
+	}
+	if alpn, ok := q["alpn"]; ok {
+		p.Alpn = strings.Split(alpn[0], ",")
+	}
+	if sni, ok := q["sni"]; ok {
+		p.Sni = sni[0]
+	}
+	if scv, ok := q["skip-cert-verify"]; ok && len(scv) > 0 && (scv[0] == "1" || scv[0] == "true") {
+		p.SkipCertVerify = clash.MyBool(true)
+	}
+	if fp, ok := q["fingerprint"]; ok {
+		p.Fingerprint = fp[0]
+	}
+	if fp, ok := q["fp"]; ok {
+		p.Fingerprint = fp[0]
+	}
+	if cfp, ok := q["client-fingerprint"]; ok {
+		p.ClientFingerprint = cfp[0]
+	}
+	if allowInsecure, ok := q["allowInsecure"]; ok && len(allowInsecure) > 0 && (allowInsecure[0] == "1" || allowInsecure[0] == "true") {
+		p.SkipCertVerify = clash.MyBool(true)
+	}
+	return p, nil
+}
+
+func parseVless(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Type:   "vless",
+		Name:   u.Fragment,
+		Server: u.Hostname(),
+		Port:   u.Port(),
+		Uuid:   u.User.String(),
+	}
+	q := u.Query()
+	if security, ok := q["security"]; ok && len(security) > 0 && security[0] != "none" {
+		p.Tls = clash.MyBool(true)
+		if security[0] == "reality" {
+			p.RealityOpts.PublicKey = q.Get("pbk")
+			p.RealityOpts.ShortId = q.Get("sid")
+		}
+	}
+
+	p.Servername = q.Get("sni")
+	if p.Servername == "" {
+		p.Servername = q.Get("peer")
+	}
+	p.Flow = q.Get("flow")
+	p.ClientFingerprint = q.Get("fp")
+	if alpn, ok := q["alpn"]; ok {
+		p.Alpn = strings.Split(alpn[0], ",")
+	}
+	if scv, ok := q["allowinsecure"]; ok && len(scv) > 0 && (scv[0] == "1" || strings.ToLower(scv[0]) == "true") {
+		p.SkipCertVerify = clash.MyBool(true)
+	}
+	p.Network = q.Get("type")
+
+	switch p.Network {
+	case "ws", "http", "grpc", "h2":
+		host := q.Get("host")
+		if host == "" {
+			host = q.Get("obfsparam")
+		}
+		path := q.Get("path")
+		switch p.Network {
+		case "ws":
+			p.WsOpts.Path = path
+			if host != "" {
+				p.WsOpts.Headers = map[string]string{
+					"Host": host,
+				}
+			}
+			if q.Get("headerType") == "http" {
+				p.WsOpts.V2rayHttpUpgrade = clash.MyBool(true)
+			}
+		case "http":
+			p.HTTPOpts.Path = []string{path}
+			if host != "" {
+				p.HTTPOpts.Headers = map[string][]string{
+					"Host": {host},
+				}
+			}
+		case "h2":
+			p.H2Opts.Path = path
+			if host != "" {
+				p.H2Opts.Host = []string{host}
+			}
+		case "grpc":
+			p.GrpcOpts.GrpcServiceName = path
+		}
+	}
+	if p.Tls && p.Servername == "" {
+		if host, ok := p.WsOpts.Headers["Host"]; ok {
+			p.Servername = host
+		} else if len(p.HTTPOpts.Headers["Host"]) > 0 {
+			p.Servername = p.HTTPOpts.Headers["Host"][0]
+		}
+	}
+
+	return p, nil
+}
+
+func parseVmess(s string) (clash.Proxies, error) {
+	s = strings.TrimPrefix(s, "vmess://")
+	b, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return clash.Proxies{}, err
+	}
+	v := struct {
+		Ps   string `json:"ps"`
+		Add  string `json:"add"`
+		Port any    `json:"port"`
+		Id   string `json:"id"`
+		Aid  any    `json:"aid"`
+		Scy  string `json:"scy"`
+		Net  string `json:"net"`
+		Type string `json:"type"`
+		Host string `json:"host"`
+		Path string `json:"path"`
+		Tls  string `json:"tls"`
+		Sni  string `json:"sni"`
+		Alpn string `json:"alpn"`
+		Fp   string `json:"fp"`
+	}{}
+	err = json.Unmarshal(b, &v)
+	if err != nil {
+		return clash.Proxies{}, err
+	}
+	name := v.Ps
+	p := clash.Proxies{
+		Type:   "vmess",
+		Name:   name,
+		Server: v.Add,
+		Uuid:   v.Id,
+		Cipher: v.Scy,
+	}
+
+	switch port := v.Port.(type) {
+	case string:
+		p.Port = port
+	case float64:
+		p.Port = strconv.FormatFloat(port, 'f', -1, 64)
+	}
+
+	switch aid := v.Aid.(type) {
+	case string:
+		i, err := strconv.Atoi(aid)
+		if err == nil {
+			p.AlterId = clash.MyInt(i)
+		}
+	case float64:
+		p.AlterId = clash.MyInt(int(aid))
+	}
+
+	if v.Tls == "tls" {
+		p.Tls = clash.MyBool(true)
+		p.Servername = v.Sni
+	}
+
+	p.Network = v.Net
+	switch p.Network {
+	case "ws":
+		p.WsOpts.Path = v.Path
+		p.WsOpts.Headers = map[string]string{
+			"Host": v.Host,
+		}
+	case "http":
+		p.HTTPOpts.Path = []string{v.Path}
+		p.HTTPOpts.Headers = map[string][]string{
+			"Host": {v.Host},
+		}
+	case "h2":
+		p.H2Opts.Path = v.Path
+		p.H2Opts.Host = []string{v.Host}
+	case "grpc":
+		p.GrpcOpts.GrpcServiceName = v.Path
+
+	}
+	if v.Alpn != "" {
+		p.Alpn = strings.Split(v.Alpn, ",")
+	}
+	p.ClientFingerprint = v.Fp
+	return p, nil
+}
+
+func parseSs(u *url.URL) (clash.Proxies, error) {
+	p := clash.Proxies{
+		Type:   "ss",
+		Server: u.Hostname(),
+		Port:   u.Port(),
+		Name:   u.Fragment,
+	}
+
+	decodedUserInfo, err := base64.RawURLEncoding.DecodeString(u.User.Username())
+	if err == nil {
+		parts := strings.SplitN(string(decodedUserInfo), ":", 2)
+		if len(parts) == 2 {
+			p.Cipher = parts[0]
+			p.Password = parts[1]
+		}
+	} else {
+		password, ok := u.User.Password()
+		if ok {
+			p.Password = password
+			p.Cipher = u.User.Username()
+		} else {
+			return clash.Proxies{}, fmt.Errorf("invalid ss link")
+		}
+	}
+
+	q := u.Query()
+
+	if plugin, ok := q["plugin"]; ok {
+		if len(plugin) > 0 {
+			pluginStr, err := url.QueryUnescape(plugin[0])
+			if err != nil {
+				return clash.Proxies{}, err
+			}
+			parts := strings.Split(pluginStr, ";")
+			if len(parts) < 2 {
+				return clash.Proxies{}, err
+			}
+			opts := make(map[string]string)
+			for _, part := range parts[1:] {
+				kv := strings.SplitN(part, "=", 2)
+				if len(kv) == 2 {
+					opts[kv[0]] = kv[1]
+				}
+			}
+
+			switch parts[0] {
+			case "obfs-local", "simple-obfs":
+				p.Plugin = "obfs"
+				pluginOpts := make(map[string]string)
+				pluginOpts["mode"] = opts["obfs"]
+				if host, ok := opts["obfs-host"]; ok {
+					pluginOpts["host"] = host
+				}
+				err = p.PluginOpts.Encode(pluginOpts)
+				if err != nil {
+					return clash.Proxies{}, err
+				}
+			case "v2ray-plugin":
+				p.Plugin = "v2ray-plugin"
+				pluginOpts := make(map[string]any)
+				pluginOpts["mode"] = opts["mode"]
+				if _, ok := opts["tls"]; ok {
+					pluginOpts["tls"] = true
+				}
+				pluginOpts["host"] = opts["host"]
+				err = p.PluginOpts.Encode(pluginOpts)
+				if err != nil {
+					return clash.Proxies{}, err
+				}
+
+			}
+		}
+	}
+
+	if tfo, ok := q["tfo"]; ok && len(tfo) > 0 && (tfo[0] == "1" || tfo[0] == "true") {
+		p.Tfo = true
+	}
+	return p, nil
+}
--- httputils/clash.go
+++ httputils/clash.go
@@ -7,12 +7,12 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"strconv"
 	"strings"
 	"sync"
 
 	"github.com/samber/lo"
 	"github.com/tidwall/gjson"
+	"github.com/xmdhs/clash2singbox/convert"
 	"github.com/xmdhs/clash2singbox/model/clash"
 	"golang.org/x/sync/errgroup"
 	"gopkg.in/yaml.v3"
@@ -44,36 +44,35 @@ func GetAny(ctx context.Context, hc *http.Client, u string, addTag bool) (clash.
 			return c, nil, nil, fmt.Errorf("GetAny: %w", err)
 		}
 		host := u.Host
-		if u.Scheme != "http" && u.Scheme != "https" {
-			node, err := parseNodeLink(v)
-			if err != nil {
-				return c, nil, nil, fmt.Errorf("GetAny: %w", err)
-			}
-			if tag, ok := node["tag"].(string); ok {
-				l.Lock()
-				singList = append(singList, node)
-				tags = append(tags, tag)
-				l.Unlock()
-			}
-			continue
-		}
 
 		g.Go(func() error {
-			b, err := HttpGet(ctx, hc, v, 1000*1000*10)
-			if err != nil {
-				return err
-			}
 			lc := clash.Clash{}
-			err = yaml.Unmarshal(b, &lc)
-			if err != nil || len(lc.Proxies) == 0 {
-				s, t, err := getSing(b, host, addTag)
+			if u.Scheme != "http" && u.Scheme != "https" {
+				node, err := convert.ParseURL(v)
+				if err != nil {
+					return fmt.Errorf("GetAny: %w", err)
+				}
+				lc.Proxies = append(c.Proxies, node)
+			} else {
+				b, err := HttpGet(ctx, hc, v, 1000*1000*10)
 				if err != nil {
 					return err
 				}
-				l.Lock()
-				singList = append(singList, s...)
-				tags = append(tags, t...)
-				l.Unlock()
+				tc := clash.Clash{}
+				err = yaml.Unmarshal(b, &tc)
+				if err != nil || len(tc.Proxies) == 0 {
+					s, t, list, err := getSing(b, host, addTag)
+					if err != nil {
+						return err
+					}
+					l.Lock()
+					singList = append(singList, s...)
+					tags = append(tags, t...)
+					l.Unlock()
+					lc.Proxies = append(lc.Proxies, list...)
+				} else {
+					lc.Proxies = append(lc.Proxies, tc.Proxies...)
+				}
 			}
 			if addTag {
 				lc.Proxies = lo.Map(lc.Proxies, func(item clash.Proxies, index int) clash.Proxies {
@@ -102,7 +101,7 @@ func GetAny(ctx context.Context, hc *http.Client, u string, addTag bool) (clash.
 
 var ErrJson = errors.New("错误的格式")
 
-func getSing(config []byte, host string, addTag bool) ([]map[string]any, []string, error) {
+func getSing(config []byte, host string, addTag bool) ([]map[string]any, []string, []clash.Proxies, error) {
 	// 首先尝试解析为 JSON 格式的 sing-box 配置
 	if gjson.Valid(string(config)) {
 		out := gjson.GetBytes(config, "outbounds").Array()
@@ -127,14 +126,14 @@ func getSing(config []byte, host string, addTag bool) ([]map[string]any, []strin
 				}
 			}
 		}
-		return outList, tagsList, nil
+		return outList, tagsList, nil, nil
 	}
 
 	// 如果不是 JSON，尝试解析为订阅链接格式
 	// 订阅链接通常是 Base64 编码的，每行一个节点链接
 	content := strings.TrimSpace(string(config))
 	if content == "" {
-		return nil, nil, fmt.Errorf("getSing: 内容为空: %w host: %v", ErrJson, host)
+		return nil, nil, nil, fmt.Errorf("getSing: 内容为空: %w host: %v", ErrJson, host)
 	}
 
 	// 尝试 Base64 解码
@@ -146,39 +145,26 @@ func getSing(config []byte, host string, addTag bool) ([]map[string]any, []strin
 
 	// 按行分割，处理每个节点链接
 	lines := strings.Split(strings.TrimSpace(string(decoded)), "\n")
-	outList := make([]map[string]any, 0)
-	tagsList := make([]string, 0)
+	outList := make([]clash.Proxies, 0)
 
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 		if line == "" {
 			continue
 		}
-
 		// 解析节点链接并转换为 sing-box 格式
-		node, err := parseNodeLink(line)
+		node, err := convert.ParseURL(line)
 		if err != nil {
 			continue // 跳过无法解析的节点
 		}
-
-		// 添加主机标签
-		if addTag {
-			if tag, ok := node["tag"].(string); ok {
-				node["tag"] = fmt.Sprintf("%s[%s]", tag, host)
-			}
-		}
-
 		outList = append(outList, node)
-		if tag, ok := node["tag"].(string); ok {
-			tagsList = append(tagsList, tag)
-		}
 	}
 
 	if len(outList) == 0 {
-		return nil, nil, fmt.Errorf("getSing: %w host: %v", ErrJson, host)
+		return nil, nil, nil, fmt.Errorf("getSing: %w host: %v", ErrJson, host)
 	}
 
-	return outList, tagsList, nil
+	return nil, nil, outList, nil
 }
 
 var notNeedType = map[string]struct{}{
@@ -187,160 +173,4 @@ var notNeedType = map[string]struct{}{
 	"dns":      {},
 	"selector": {},
 	"urltest":  {},
-}
-
-// parseNodeLink 解析节点链接并转换为 sing-box outbound 格式
-func parseNodeLink(link string) (map[string]any, error) {
-	u, err := url.Parse(link)
-	if err != nil {
-		return nil, err
-	}
-
-	switch u.Scheme {
-	case "trojan":
-		return parseTrojanLink(u)
-	case "vmess":
-		return parseVmessLink(u)
-	case "vless":
-		return parseVlessLink(u)
-	case "ss":
-		return parseShadowsocksLink(u)
-	default:
-		return nil, fmt.Errorf("unsupported protocol: %s", u.Scheme)
-	}
-}
-
-// parseTrojanLink 解析 Trojan 链接
-func parseTrojanLink(u *url.URL) (map[string]any, error) {
-	password := u.User.Username()
-	host := u.Hostname()
-	port := u.Port()
-
-	node := map[string]any{
-		"type":        "trojan",
-		"tag":         getNameFromURL(u),
-		"server":      host,
-		"server_port": parsePort(port),
-		"password":    password,
-		"network":     []string{"tcp", "udp"}, // trojan协议支持TCP和UDP
-	}
-
-	// 解析查询参数
-	query := u.Query()
-
-	// 处理TLS配置
-	tls := map[string]any{
-		"enabled": true,
-	}
-
-	// 设置SNI
-	if sni := query.Get("sni"); sni != "" {
-		tls["server_name"] = sni
-	} else {
-		// 如果没有SNI，使用host作为server_name
-		tls["server_name"] = host
-	}
-
-	// 处理peer参数（可能用于TLS验证）
-	if peer := query.Get("peer"); peer != "" {
-		// peer参数通常用于指定证书验证的主机名
-		tls["server_name"] = peer
-	}
-
-	node["tls"] = tls
-
-	return node, nil
-}
-
-// parseVmessLink 解析 Vmess 链接
-func parseVmessLink(u *url.URL) (map[string]any, error) {
-	// Vmess 链接通常是 Base64 编码的 JSON，需要额外处理
-	return nil, fmt.Errorf("vmess parsing not implemented yet")
-}
-
-// parseVlessLink 解析 Vless 链接
-func parseVlessLink(u *url.URL) (map[string]any, error) {
-	uuid := u.User.Username()
-	host := u.Hostname()
-	port := u.Port()
-
-	node := map[string]any{
-		"type":        "vless",
-		"tag":         getNameFromURL(u),
-		"server":      host,
-		"server_port": parsePort(port),
-		"uuid":        uuid,
-		"network":     []string{"tcp", "udp"}, // vless协议支持TCP和UDP
-	}
-
-	// 解析查询参数
-	query := u.Query()
-	if sni := query.Get("sni"); sni != "" {
-		node["tls"] = map[string]any{
-			"enabled":     true,
-			"server_name": sni,
-		}
-	}
-
-	return node, nil
-}
-
-// parseShadowsocksLink 解析 Shadowsocks 链接
-func parseShadowsocksLink(u *url.URL) (map[string]any, error) {
-	host := u.Hostname()
-	port := u.Port()
-
-	// Shadowsocks 链接的格式: ss://base64(method:password)@host:port#name
-	userInfo, err := base64.StdEncoding.DecodeString(u.User.String())
-	if err != nil {
-		return nil, err
-	}
-
-	parts := strings.SplitN(string(userInfo), ":", 2)
-	if len(parts) != 2 {
-		return nil, fmt.Errorf("invalid shadowsocks user info")
-	}
-
-	method := parts[0]
-	password := parts[1]
-
-	node := map[string]any{
-		"type":        "shadowsocks",
-		"tag":         getNameFromURL(u),
-		"server":      host,
-		"server_port": parsePort(port),
-		"method":      method,
-		"password":    password,
-		"network":     []string{"tcp", "udp"}, // shadowsocks协议支持TCP和UDP
-	}
-
-	return node, nil
-}
-
-// getNameFromURL 从 URL 中提取节点名称
-func getNameFromURL(u *url.URL) string {
-	if u.Fragment != "" {
-		// URL 解码片段部分
-		if name, err := url.QueryUnescape(u.Fragment); err == nil {
-			return name
-		}
-		return u.Fragment
-	}
-	// 如果没有片段，使用主机名作为名称
-	return u.Hostname()
-}
-
-// parsePort 解析端口字符串为整数
-func parsePort(portStr string) int {
-	if portStr == "" {
-		return 443 // 默认端口
-	}
-
-	// 使用strconv.Atoi进行更可靠的转换
-	if port, err := strconv.Atoi(portStr); err == nil && port > 0 && port <= 65535 {
-		return port
-	}
-
-	// 如果转换失败，返回默认端口
-	return 443
 }
